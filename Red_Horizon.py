02fa06eec8cf5685abfd6444e81492ab7aeee738
Daniil10001 <daniiltimohin8@gmail.com>
2023-03-14 15:23:27 +0300

    hello rospy

--- Red_Horizon.py
+++ Red_Horizon.py
@@ -1,1 +1,15 @@
-print("hello_world")
+import rospy
+from std_msgs.msg import String
+
+
+rospy.init_node('talker', anonymous=True)
+pub = rospy.Publisher('chatter', String, queue_size=10)
+rospy.Rate(10) # 10hz
+
+   
+if __name__ == '__main__':
+    while not rospy.is_shutdown():
+         hello_str = "hello world %s" % 1
+         rospy.loginfo(hello_str)
+         pub.publish(hello_str)
+         rospy.sleep()

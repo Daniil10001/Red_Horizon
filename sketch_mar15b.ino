071546be08761139fecc8d4d14f19cc423e1be4c
MrFen1x <61620841+MrFen1x@users.noreply.github.com>
2023-03-16 18:48:05 +0300

    Update sketch_mar15b.ino

--- sketch_mar15b.ino
+++ sketch_mar15b.ino
@@ -1,13 +1,13 @@
 #include <ros.h>
-#include <std_msgs/Int32.h>
+#include <std_msgs/UInt16.h>
 #include "Adafruit_VL53L1X.h"
-
+#define USE_USBCON
 #define IRQ_PIN 2
 #define XSHUT_PIN 3
 ros::NodeHandle nh;
 
-std_msgs::String str_msg;
-ros::Publisher range("chatter", Int32);
+std_msgs::UInt16 Int;
+ros::Publisher range("range",&Int);
 
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
 void setup()
@@ -18,13 +18,17 @@ void setup()
 }
 
 void loop()
-{  int16_t distance;
+{  
+  int16_t distance;
+
 
   if (vl53.dataReady()) {
     // new measurement for the taking!
     distance = vl53.distance();
     
-  str_msg.data = distance;
-  range.publish(Int32);
+  Int.data = distance;
+  range.publish( Int.data );
   nh.spinOnce();
   delay(1000);
+}
+}
